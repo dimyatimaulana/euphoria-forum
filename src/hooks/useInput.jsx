5fe593cc3af3295da8eec54e1e7637e84c8dbce8
maulana dimyati <dimyatimaulana@gmail.com>
2023-02-23 20:27:54 +0700

    error test

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -3,11 +3,12 @@ import { useState } from 'react';
 function useInput(defaultValue = '') {
   const [value, setValue] = useState(defaultValue);
 
+  // eslint-disable-next-line no-unused-vars
   function handleValueChange({ target }) {
     setValue(target.value);
   }
 
-  return [value, handleValueChange, setValue];
+  return [value, setValue];
 }
 
 export default useInput;
